91863514d23cf02dc6f2565e36054d67e42689f9
gourabofficial <gangulygourab3@gmail.com>
2024-09-10 00:06:52 +0530

    sefine autoboxing and unboxing (Wrapper class in java ) important topic

--- 01_Questio./AutoBoxingAndUnboxing.java
+++ 01_Questio./AutoBoxingAndUnboxing.java
@@ -1,7 +1,13 @@
+
+
 public class AutoBoxingAndUnboxing {
     public static void main(String[] args) {
         Integer a = 5; // auto boxing 
+        int b = a; // unboxing 
         System.out.println(a);
-        int b = a; // unboxing
+        
+        Double value1 = 7954795.759;
+        double value2 = value1;
+        System.out.println(value1);
     }
 }
