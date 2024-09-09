efa3ae56aaaf69bb205e213da16b9795d1e9d37c
gourabofficial <gangulygourab3@gmail.com>
2024-09-09 23:57:29 +0530

    Add AutoBoxingAndUnboxing example

---
+++ 01_Questio./AutoBoxingAndUnboxing.java
@@ -0,0 +1,7 @@
+public class AutoBoxingAndUnboxing {
+    public static void main(String[] args) {
+        Integer a = 5; // auto boxing 
+        System.out.println(a);
+        int b = a; // unboxing
+    }
+}
