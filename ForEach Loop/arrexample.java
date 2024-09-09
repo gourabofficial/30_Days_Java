9bbc06243c88907296dc0b15fa1b764239b8dc45
gourabofficial <gangulygourab3@gmail.com>
2024-09-09 23:29:14 +0530

    foreach loop

---
+++ ForEach Loop/arrexample.java
@@ -0,0 +1,11 @@
+public class arrexample {
+
+    public static void main(String[] args) {
+        String arr[] = { "mango", "apple", "banana", "lichi", "Crinstiano Ronaldo" };
+        for (String str : arr) {
+            System.out.println(str);    //for (type variableName : arrayName) 
+                
+              
+        }
+    }
+}
