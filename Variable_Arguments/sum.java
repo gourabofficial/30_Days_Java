abab81b299b5204d67d526fc2acf2a67fb93e166
gourabofficial <gangulygourab3@gmail.com>
2024-09-09 23:02:05 +0530

    variable arguments

---
+++ Variable_Arguments/sum.java
@@ -0,0 +1,19 @@
+
+
+public class sum {
+
+    public static void main(String[] args) {
+
+        int result = sum(1, 2, 3, 4, 5);
+        System.out.println("sum is: " +result);
+    }
+
+    public static int sum(int a, int b, int ... c) {
+        int sum = a + b;
+        for (int i : c) {
+            sum = sum + i;
+          
+        }
+        return sum;
+    }
+}
