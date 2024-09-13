1c722953d553db7f2b8ac5f19feeea13966d80a2
gourabofficial <gangulygourab3@gmail.com>
2024-09-13 12:40:44 +0530

    updated

--- 01_Questio./EvenOrOdd.java
+++ 01_Questio./EvenOrOdd.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class EvenOrOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number for checking: ");
+        System.out.print("Enter the number for checking: ");
         int number = sc.nextInt();
 
         if ((number & 1) == 0) {
--- VariableArguments/sum.java
+++ VariableArguments/sum.java
@@ -1,5 +1,3 @@
-
-
 public class sum {
 
     public static void main(String[] args) {
